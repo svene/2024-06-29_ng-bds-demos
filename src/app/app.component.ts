abc9dda3b4f8729e1fbd56bbcf7be68b04c01d1d
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-06-30 07:56:51 +0200

    app: setup navigation with bal-tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {BalApp, BalButton, BalFooter, BalHeading} from "@baloise/ds-angular";
+import {RouterOutlet} from '@angular/router';
+import {BalApp, BalButton, BalFooter, BalHeading, BalTabItem, BalTabs} from "@baloise/ds-angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RouterOutlet, BalApp, BalHeading, BalButton, BalFooter],
+  imports: [RouterOutlet, BalApp, BalHeading, BalButton, BalFooter,  BalTabItem, BalTabs],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '2024-06-29_ng-bds-demos';
+
 }
