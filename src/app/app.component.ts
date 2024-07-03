0470ad7d86e3b2aa3376caa92dfc8036c67d22bb
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-07-03 06:24:09 +0200

    SignalInputDemo: created structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {BalApp, BalButton, BalFooter, BalHeading, BalTabItem, BalTabs} from "@baloise/ds-angular";
 import {DestroyDemoComponent} from "./demos/destroy-demo/destroy-demo.component";
+import {SignalInputDemoComponent} from "./demos/signal-input-demo/signal-input-demo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RouterOutlet, BalApp, BalHeading, BalButton, BalFooter, BalTabItem, BalTabs, DestroyDemoComponent],
+  imports: [RouterOutlet, BalApp, BalHeading, BalButton, BalFooter, BalTabItem, BalTabs, DestroyDemoComponent, SignalInputDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
