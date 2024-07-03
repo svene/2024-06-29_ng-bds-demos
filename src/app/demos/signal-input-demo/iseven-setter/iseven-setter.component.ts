c62a17f7bc0e1f792acd53dc792347464ce1fd1b
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-07-03 06:40:15 +0200

    SignalInputDemo: setter component done

--- src/app/demos/signal-input-demo/iseven-setter/iseven-setter.component.ts
+++ src/app/demos/signal-input-demo/iseven-setter/iseven-setter.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {BalCard} from "@baloise/ds-angular";
+import {count} from "rxjs";
 
 @Component({
   selector: 'app-iseven-setter',
@@ -9,10 +10,17 @@ import {BalCard} from "@baloise/ds-angular";
   ],
   template: `
     <bal-card>
-      <p>iseven-setter works!</p>
+      <p>iseven-setter: isEven: {{isEven}}</p>
     </bal-card>
   `,
 })
 export class IsevenSetterComponent {
+  isEven: boolean | undefined;
 
+  @Input({required: true})
+  set counter(c: number) {
+    this.isEven = c % 2 === 0;
+  }
+
+  // protected readonly count = count;
 }
--- src/app/demos/signal-input-demo/signal-input-demo.component.ts
+++ src/app/demos/signal-input-demo/signal-input-demo.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import {IsevenSetterComponent} from "./iseven-setter/iseven-setter.component";
+import {interval} from "rxjs";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-signal-input-demo',
   standalone: true,
   imports: [
-    IsevenSetterComponent
+    IsevenSetterComponent,
+    AsyncPipe,
+    NgIf
   ],
   template: `
-    <p>signal-input-demo works!</p>
-    <app-iseven-setter></app-iseven-setter>
+    <ng-container *ngIf="numbers$ | async as number">
+      <h1>signal-input-demo</h1>
+      <div>counter: {{number}}</div>
+
+      <app-iseven-setter [counter]="number"></app-iseven-setter>
+    </ng-container>
   `,
 })
 export class SignalInputDemoComponent {
+
+  numbers$ = interval(1000)
 
 }
